6781999a12d79a860a64df4fd256abd5306e17c2
bellalau <45056367+bellalau@users.noreply.github.com>
2020-10-01 18:17:08 -0700

    Update wnbachamp.js
    change var to const

--- wnbachamp.js
+++ wnbachamp.js
@@ -2,7 +2,7 @@
 //const points = 1, threes = .5, rebound = 1.25, assist = 1.5, steal = 2, block = 2, to = -.5, dd = 1.5, td = 3;
 //let player = { points: 0, threes: 0, rebound: 0, assist: 0, steal: 0, block: 0, to: 0, dd: 0, td: 0};
 
-var seattle = [
+const seattle = [
 	{name: 'BStew', points: 23, threes: 1.3, rebound: 8, assist: 5.7, steal: 2.3, block: 1.7, to: 1.3, dd: 1, td: 0},
 	{name: 'JLoyd', points: 17.3, threes: 2.7, rebound: 5, assist: 3.7, steal: .7, block: .7, to: 3, dd: 0, td: 0},
 	{name: 'AClark', points: 9, threes: 1, rebound: 6.7, assist: 2, steal: .7, block: .3, to: .7, dd: 0, td: 0},
@@ -13,9 +13,7 @@ var seattle = [
 	{name: 'SWhitcomb', points: 5, threes: 1, rebound: 1.3, assist: 1.7, steal: 1, block: 0, to: .3, dd: 0, td: 0},
 	{name: 'EPrince', points: 6, threes: 1, rebound: 1, assist: 2, steal: 0, block: 0, to: 0, dd: 0, td: 0},
 	{name: 'EMagbegor', points: 1.3, threes: 0, rebound: 2, assist: .3, steal: 0, block: .3, to: 0, dd: 0, td: 0}
-];
-
-var lv = [
+], lv = [
 	{name: 'AWilson', points: 21.8, threes: 0, rebound: 10.4, assist: 2.6, steal: .8, block: 3, to: 2.8, dd: 3, td: 0},
 	{name: 'KMcBride', points: 8, threes: .4, rebound: 3.8, assist: 2.4, steal: .4, block: 0, to: 1.2, dd: 0, td: 0},
 	{name: 'DRobinson', points: 10.2, threes: .2, rebound: 4.4, assist: 3, steal: 1.2, block: 0, to: 1.2, dd: 0, td: 0},
@@ -56,6 +54,3 @@ lv.forEach(x => {
 results.sort((a,b) => b.score - a.score);
 
 console.log(results);
-
-//LV.sort((a,b) => b.score - a.score);
-//Natasha Howard
